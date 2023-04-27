84a89a7b8c815d2e0dd961f22129d0edc2308cb9
SuperYuIuo <39399442+SuperYuIuo@users.noreply.github.com>
2023-04-27 20:57:18 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+
 # 设置变量
 repo_url="git@github.com:spiritLHLS/auto-push.git" #改成你的SSH远程地址
 commit_message="Auto update" # 提交时相应的说明
