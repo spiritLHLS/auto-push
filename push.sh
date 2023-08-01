411afa833715d202778e0eb89b89a368af37f0c7
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-01 12:08:06 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -39,7 +39,7 @@ git pull origin $branch --rebase
 cp 你的文件的绝对路径 ./$remote_folder
 # cp -r 你的文件夹的绝对路径 ./$remote_folder
 # 或者下面这个直接在对应文件夹内建立push.sh文件，自行替换/root/test/为你要传的文件夹
-# rsync -av --exclude='push.sh' /root/test/ ./$remote_folder
+# rsync -av --exclude='push.sh' --exclude='temp' /root/test/ ./$remote_folder
 
 # 添加所有文件到暂存区
 git add .
